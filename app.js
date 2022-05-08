164ab69f0aee9853080399c37f3eeb02c82ceb18
just-arun <arunberry47@gmail.com>
2022-05-08 14:45:05 +0530

    adding path

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 
 const writeFile = (fileType, data) => {
 	return new Promise((res, rej) => {
-		fs.writeFile(path.join(__dirname, `pub/${fileType}/${Date.now()}.json`), JSON.stringify({ data: data.split(',')[1] }), (err) => {
+		fs.writeFile(`${fileType}/${Date.now()}.json`, JSON.stringify({ data: data.split(',')[1] }), (err) => {
 			if (err) {
 				return rej(err)
 			} return res("saved")
@@ -25,15 +25,38 @@ const writeFile = (fileType, data) => {
 	})
 }
 
+const makeFolderIfNotExists = (pathName) => {
+	if (!fs.existsSync(pathName)) {
+		fs.mkdirSync(pathName);
+	}
+}
+
 app.post('/save', async (req, res) => {
 	try {
 		let body = req.body;
+		console.log(body.folder, body.type);
 		if (!!body.files) {
-			let pathName = path.join(__dirname, `pub/${body.type}`)
-			if (!fs.existsSync(pathName)) {
-				fs.mkdirSync(pathName);
+			let pathName = (f) => path.join(__dirname, `pub/${f}`)
+			// if (!fs.existsSync(pathName(body.type))) {
+			// 	fs.mkdirSync(pathName(body.type));
+			// }
+			makeFolderIfNotExists(pathName(body.type));
+			let pathList = String(body.folder).split('/');
+			console.log(pathList);
+			let fPath = body.type;
+			console.log(fPath);
+			for (let i = 0; i < pathList.length; i++) {
+				const e = pathList[i];
+				if (i == 0) {
+					fPath = pathName(`${body.type}/${e}`)
+					makeFolderIfNotExists(fPath)
+				} else {
+					fPath = pathName(`${body.type}/${[...pathList].splice(0, i).join('/')}`)
+					makeFolderIfNotExists(fPath)
+				}
+				console.log(fPath);
 			}
-			return await Promise.all(body.files.map(async (file) => await writeFile(body.type, file)))
+			return await Promise.all(body.files.map(async (file) => await writeFile(fPath, file)))
 				.then(_ => {
 					return res.status(200).json({ data: "ok" })
 				}).catch(err => {
@@ -91,6 +114,13 @@ app.post('/read', (req, res) => {
 	console.log(body);
 	const fil = require(`./${body.path}`);
 	console.log();
+	if (/image/i.test(body.path)) {
+		res.render("image", {
+			enc: body.enc,
+			data: fil.data
+		})
+		return
+	}
 	res.render("video", {
 		enc: body.enc,
 		data: fil.data
